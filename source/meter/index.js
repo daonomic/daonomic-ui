0638edc2c630008dfadefe0724b0892374e7f239
Dev <dev@0v1se.com>
2017-12-10 16:08:02 +0300

    Fix meter border radius

--- source/meter/index.js
+++ source/meter/index.js
@@ -4,15 +4,12 @@ import cn from 'classnames';
 import styles from './meter.css';
 
 export default function Meter({ className, value }) {
-  const meterHeight = 12;
-
   return (
-    <svg className={cn(className, styles.root)} height={meterHeight}>
+    <svg className={cn(className, styles.root)}>
       <rect
         className={cn(styles.bar, styles.bar_background)}
         x="0"
         y="0"
-        rx={meterHeight / 2}
         width="100%"
         height="100%"
       />
@@ -21,7 +18,6 @@ export default function Meter({ className, value }) {
         className={cn(styles.bar, styles.bar_foreground)}
         x="0"
         y="0"
-        rx={meterHeight / 2}
         width={`${value * 100}%`}
         height="100%"
       />
