5e581f3eb722044a0dbaec489f84670b245a69e5
Dev <dev@0v1se.com>
2017-12-17 12:24:36 +0300

    Update Meter design, v1.1.0

--- source/meter/index.js
+++ source/meter/index.js
@@ -5,23 +5,14 @@ import styles from './meter.css';
 
 export default function Meter({ className, value }) {
   return (
-    <svg className={cn(className, styles.root)}>
-      <rect
-        className={cn(styles.bar, styles.bar_background)}
-        x="0"
-        y="0"
-        width="100%"
-        height="100%"
-      />
+    <div className={cn(className, styles.root)}>
+      <div className={cn(styles.bar, styles.bar_background)} />
 
-      <rect
+      <div
         className={cn(styles.bar, styles.bar_foreground)}
-        x="0"
-        y="0"
-        width={`${value * 100}%`}
-        height="100%"
+        style={{ width: `${Math.max(value * 100, 1)}%` }}
       />
-    </svg>
+    </div>
   );
 }
 
