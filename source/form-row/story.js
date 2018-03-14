08172cb604d5dd8236cdf19aebae57407b01c4a9
Dev <dev@0v1se.com>
2018-03-14 17:11:50 +0300

    Update FormRow example

--- source/form-row/story.js
+++ source/form-row/story.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import FormRow from './';
-import Input from '../input';
-import Button from '../button';
 
 storiesOf('FormRow', module).add('default', () => (
   <React.Fragment>
-    <FormRow>
-      <Input.Uncontrolled label="Name" />
-    </FormRow>
-
-    <FormRow>
-      <Input.Uncontrolled label="E-mail" />
-    </FormRow>
-
-    <FormRow>
-      <Button>Submit</Button>
-    </FormRow>
+    <FormRow>row</FormRow>
+    <FormRow>row</FormRow>
+    <FormRow>row</FormRow>
   </React.Fragment>
 ));
