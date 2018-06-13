55754f5966668103a1d29d1c2c339675eee07b40
Dev <dev@0v1se.com>
2018-06-13 17:48:21 +0300

    Add MailtoLink component

---
+++ source/components/mailto-link/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function encode(string) {
+  if (typeof window !== 'undefined' && typeof window.btoa === 'function') {
+    return window.btoa(string);
+  }
+
+  return string;
+}
+
+function decode(encodedString) {
+  if (typeof window !== 'undefined' && typeof window.atob === 'function') {
+    const decodedHref = window.atob(encodedString);
+    const larr = decodedHref.split('#')[1].split('|');
+    const darr = larr[1].split('%');
+
+    return `mailto:${larr[0]}@${darr[0]}.${darr[1]}`;
+  }
+
+  return encodedString;
+}
+
+export default class MailtoLink extends React.Component {
+  static propTypes = {
+    href: PropTypes.string.isRequired,
+    element: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+    children: PropTypes.node,
+  };
+
+  static defaultProps = {
+    element: 'a',
+  };
+
+  isDecoded = false;
+
+  handleClick = (event) => {
+    if (this.isDecoded) {
+      return;
+    }
+
+    event.target.href = decode(event.target.getAttribute('href').slice(1));
+    this.isDecoded = true;
+  };
+
+  render() {
+    const { href, children, element, ...restProps } = this.props;
+
+    return React.createElement(
+      element,
+      {
+        ...restProps,
+        href: this.isDecoded ? href : `#${encode(href)}`,
+        onClick: this.handleClick,
+      },
+      children,
+    );
+  }
+}
---
+++ source/components/mailto-link/story.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import MailtoLink from './';
+
+storiesOf('MailtoLink', module).add('default', () => (
+  <MailtoLink href="#example|example%com">Contact us</MailtoLink>
+));
