47ac886d6b6dad0e234f9306121e238771799d45
Dev <dev@0v1se.com>
2018-11-27 16:40:05 +0300

    Add modal component

---
+++ source/components/modal/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import ReactModal from 'react-modal';
+import Text from '../text';
+import styles from './styles.css';
+
+export default function Modal({
+  children,
+  className,
+  title,
+  onClose,
+  ...restProps
+}) {
+  return (
+    <ReactModal
+      contentLabel={title}
+      {...restProps}
+      className={cn(className, styles.window)}
+      overlayClassName={styles.overlay}
+    >
+      <header className={styles.header}>
+        <Text element="h2" size="xl">
+          {title}
+        </Text>
+        <button className={styles.close} onClick={onClose}>
+          Close
+        </button>
+      </header>
+
+      <div className={styles.content}>{children}</div>
+    </ReactModal>
+  );
+}
+
+Modal.propTypes = {
+  children: PropTypes.node.isRequired,
+  title: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
+  className: PropTypes.string,
+};
---
+++ source/components/modal/story.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { State, Store } from '@sambego/storybook-state';
+import Modal from '.';
+
+const store = new Store({
+  isOpen: false,
+});
+
+storiesOf('Modal', module).add('default', () => (
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, ipsa
+      autem facilis sequi dolor quas saepe soluta atque in cum, temporibus
+      officia, dolorem quibusdam aperiam qui magni vero! Ipsam, illo?
+    </p>
+
+    <button onClick={() => store.set({ isOpen: true })}>Open modal</button>
+
+    <State store={store}>
+      {(state) => (
+        <Modal
+          isOpen={state.isOpen}
+          title="Modal"
+          onClose={() => store.set({ isOpen: false })}
+        >
+          <p>Modal content</p>
+        </Modal>
+      )}
+    </State>
+  </div>
+));
