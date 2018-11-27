8b2ea012343f7b9464307ecca10b90798013d919
Dev <dev@0v1se.com>
2018-11-27 17:32:59 +0300

    Add Modal export to root

--- source/index.js
+++ source/index.js
@@ -16,3 +16,4 @@ export { default as Panel } from './components/panel';
 export { default as Select } from './components/select';
 export { default as Text } from './components/text';
 export { default as MailtoLink } from './components/mailto-link';
+export { default as Modal } from './components/modal';
