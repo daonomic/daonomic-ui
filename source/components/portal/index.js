44be79d27ce3974139e7f24222428666e5e60fe7
Dev <dev@0v1se.com>
2018-12-12 15:25:32 +0300

    Add Portal

---
+++ source/components/portal/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+export default class Portal extends React.Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.container = document.createElement('div');
+    document.body.appendChild(this.container);
+  }
+
+  render() {
+    return ReactDOM.createPortal(this.props.children, this.container);
+  }
+}
--- source/index.js
+++ source/index.js
@@ -18,3 +18,4 @@ export { default as Text } from './components/text';
 export { default as MailtoLink } from './components/mailto-link';
 export { default as Modal } from './components/modal';
 export { default as Tooltip } from './components/tooltip';
+export { default as Portal } from './components/portal';
