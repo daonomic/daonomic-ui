66bde75faed6b943241d28719f2d6582b8a341be
Dev <dev@0v1se.com>
2019-02-12 13:21:09 +0300

    Update Badge design

--- source/components/badge/story.js
+++ source/components/badge/story.js
@@ -7,17 +7,26 @@ storiesOf('Badge', module)
   .add(
     'sizes and colors',
     withInfo()(() => (
-      <React.Fragment>
+      <div style={{ padding: '1em', background: '#fff' }}>
         {sizes.map((size) => (
           <div key={size} style={{ paddingBottom: '1em' }}>
             {colors.map((color) => (
-              <Badge key={color} size={size} color={color}>
-                {size} {color}
-              </Badge>
+              <div
+                key={color}
+                style={{ display: 'inline-block', margin: '0 .25em' }}
+              >
+                <Badge size={size} color={color}>
+                  {size} {color}
+                </Badge>
+              </div>
             ))}
           </div>
         ))}
-      </React.Fragment>
+      </div>
     )),
   )
-  .add('uppercase', () => <Badge isUppercase>Badge</Badge>);
+  .add('uppercase', () => (
+    <div style={{ padding: '1em', background: '#fff' }}>
+      <Badge isUppercase>Badge</Badge>
+    </div>
+  ));
