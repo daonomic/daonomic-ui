89efae359294e79c9e395c20d7a52a451e15f2be
Dev <dev@0v1se.com>
2019-02-18 13:36:24 +0300

    Add DataTable component with empty and loading states

---
+++ source/components/data-table/index.js
@@ -0,0 +1,90 @@
+// @flow
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import { Table } from '../table';
+import { PlaceholderIllustration } from '../placeholder-illustration';
+import Spinner from '../spinner';
+import style from './style.css';
+
+export class DataTable extends React.Component {
+  static propTypes = {
+    primaryKey: PropTypes.string.isRequired,
+    schema: PropTypes.array.isRequired,
+    data: PropTypes.array.isRequired,
+    placeholder: PropTypes.node.isRequired,
+    className: PropTypes.string,
+    dataState: PropTypes.oneOf(['idle', 'loading', 'loaded', 'failed']),
+  };
+
+  static defaultProps = {
+    dataState: 'loaded',
+  };
+
+  renderPlaceholder = () => (
+    <div className={style.placeholder}>
+      <PlaceholderIllustration className={style.illustration} />
+      {this.props.placeholder}
+    </div>
+  );
+
+  renderPreloader = () => (
+    <div className={style.preloader}>
+      <Spinner />
+    </div>
+  );
+
+  render() {
+    const {
+      primaryKey,
+      schema,
+      data,
+      dataState,
+      className,
+      ...restProps
+    } = this.props;
+
+    return (
+      <div
+        className={cn(className, style.root, {
+          [style.root_loading]: dataState === 'loading',
+        })}
+        {...restProps}
+      >
+        <Table className={style.table}>
+          <Table.Thead>
+            <Table.Tr>
+              {schema.map((row) => (
+                <Table.Th key={row.name} align={row.align || 'left'}>
+                  {row.name}
+                </Table.Th>
+              ))}
+            </Table.Tr>
+          </Table.Thead>
+
+          <Table.Tbody>
+            {data.map((item) => (
+              <Table.Tr key={item[primaryKey]}>
+                {schema.map((row) => (
+                  <Table.Td
+                    key={row.name}
+                    align={row.align || 'left'}
+                    className={style.loading}
+                  >
+                    {row.render(item)}
+                  </Table.Td>
+                ))}
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+
+        {dataState === 'loaded' &&
+          data.length === 0 &&
+          this.renderPlaceholder()}
+
+        {dataState === 'loading' && this.renderPreloader()}
+      </div>
+    );
+  }
+}
---
+++ source/components/data-table/story.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import faker from 'faker';
+import { DataTable } from '.';
+
+const statuses = ['NONE', 'ON_REVIEW', 'ALLOWED', 'DENIED'];
+
+function createDemoUser() {
+  return {
+    email: faker.internet.email(),
+    bought: faker.random.number(25000),
+    reviewStatus: statuses[faker.random.number(statuses.length - 1)],
+    createDate: faker.date.between(new Date('2018-01-01'), new Date()),
+  };
+}
+
+const demoUsers = Array.from({ length: 10 }, createDemoUser);
+const schema = [
+  { name: 'Email', render: (item) => item.email },
+  { align: 'right', name: 'Bought', render: (item) => item.bought },
+  { name: 'Status', render: (item) => item.reviewStatus },
+  {
+    align: 'right',
+    name: 'Registered',
+    render: (item) => item.createDate.toLocaleDateString(),
+  },
+  {
+    align: 'right',
+    name: '',
+    render: (item) => (
+      <button onClick={() => alert(`Delete user ${item.email}`)}>×</button>
+    ),
+  },
+];
+
+storiesOf('Data table', module)
+  .add('default', () => (
+    <div style={{ padding: '2em', background: '#fff' }}>
+      <DataTable
+        primaryKey="email"
+        schema={schema}
+        data={demoUsers}
+        placeholder="Nothing found"
+      />
+    </div>
+  ))
+  .add('loading', () => (
+    <div style={{ padding: '2em', background: '#fff' }}>
+      <DataTable
+        dataState="loading"
+        primaryKey="email"
+        schema={schema}
+        data={demoUsers}
+        placeholder="Nothing found"
+      />
+    </div>
+  ))
+  .add('empty', () => (
+    <div style={{ padding: '2em', background: '#fff' }}>
+      <DataTable
+        primaryKey="email"
+        schema={schema}
+        data={[]}
+        placeholder="Nothing found"
+      />
+    </div>
+  ));
